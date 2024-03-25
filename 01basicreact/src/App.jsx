e0b8f9afbd7a3769946867157044f9cd66a22ece
SuchitaGhadge <suchita8494.sg@gmail.com>
2024-03-25 17:09:16 +0530

    counter app

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,8 +1,18 @@
-
+import { useState } from "react";
 function App() {
+const [counter, setCounter] = useState(0)
+
+const removeValue = () =>{
+  if(counter > 0){
+    setCounter(counter-1)
+  }
+} 
   return (
     <>
     <h1>Coffee and React</h1>
+    <h2>Counter value here: {counter}</h2>
+    <button onClick={() => setCounter(counter+1)}>Add value</button>
+    <button onClick={removeValue}>Remove value</button>
     </>
   )
 }
